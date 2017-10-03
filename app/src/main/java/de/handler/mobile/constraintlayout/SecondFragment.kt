4ba69dda486dddc65190889113ab94ded43b9109
Nino Handler <nih@adorsys.de>
2017-10-03 23:59:48 +0200

    Added animation for aligning both elements next to each other

--- app/src/main/java/de/handler/mobile/constraintlayout/SecondFragment.kt
+++ app/src/main/java/de/handler/mobile/constraintlayout/SecondFragment.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
-import android.support.v4.content.ContextCompat
+import android.support.constraint.solver.widgets.ConstraintWidget
 import android.transition.AutoTransition
 import android.transition.Transition
 import android.transition.TransitionManager
@@ -14,7 +14,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import de.handler.mobile.example_constraintlayout.R
-import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : BackAwareFragment() {
     private lateinit var circle: CircleView
@@ -51,20 +50,50 @@ class SecondFragment : BackAwareFragment() {
 //                animatedConstraints.centerVertically(R.id.fragment_second_switch_fragment_button, R.id.fragment_second_circle)
 
                 // grow button to full screen size
-                fragment_second_switch_fragment_button?.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+//                fragment_second_switch_fragment_button?.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+//                animatedConstraints.clear(R.id.fragment_second_switch_fragment_button)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.START,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.START, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.END,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.END, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.TOP,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.TOP, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.BOTTOM,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.BOTTOM, 0)
+
+                // create a chain of the two elements and align them next to each other
+                // reset constraints and set height / width to necessary values
                 animatedConstraints.clear(R.id.fragment_second_switch_fragment_button)
                 animatedConstraints.connect(
-                        R.id.fragment_second_switch_fragment_button, ConstraintSet.START,
-                        R.id.fragment_second_constraint_layout, ConstraintSet.START, 0)
+                        R.id.fragment_second_switch_fragment_button, ConstraintSet.LEFT,
+                        R.id.fragment_second_constraint_layout, ConstraintSet.LEFT, 0)
                 animatedConstraints.connect(
-                        R.id.fragment_second_switch_fragment_button, ConstraintSet.END,
-                        R.id.fragment_second_constraint_layout, ConstraintSet.END, 0)
+                        R.id.fragment_second_switch_fragment_button, ConstraintSet.RIGHT,
+                        R.id.fragment_second_circle, ConstraintSet.LEFT, 0)
+                animatedConstraints.constrainWidth(R.id.fragment_second_switch_fragment_button, 0)
+                animatedConstraints.constrainHeight(R.id.fragment_second_switch_fragment_button, ConstraintSet.WRAP_CONTENT)
+
+                animatedConstraints.clear(R.id.fragment_second_circle)
                 animatedConstraints.connect(
-                        R.id.fragment_second_switch_fragment_button, ConstraintSet.TOP,
-                        R.id.fragment_second_constraint_layout, ConstraintSet.TOP, 0)
+                        R.id.fragment_second_circle, ConstraintSet.LEFT,
+                        R.id.fragment_second_switch_fragment_button, ConstraintSet.RIGHT, 0)
                 animatedConstraints.connect(
-                        R.id.fragment_second_switch_fragment_button, ConstraintSet.BOTTOM,
-                        R.id.fragment_second_constraint_layout, ConstraintSet.BOTTOM, 0)
+                        R.id.fragment_second_circle, ConstraintSet.RIGHT,
+                        R.id.fragment_second_constraint_layout, ConstraintSet.RIGHT, 0)
+                animatedConstraints.constrainWidth(R.id.fragment_second_circle, 0)
+                animatedConstraints.constrainHeight(R.id.fragment_second_circle, ConstraintSet.WRAP_CONTENT)
+
+                // create chain
+                animatedConstraints.createHorizontalChain(
+                        ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
+                        ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
+                        intArrayOf(R.id.fragment_second_switch_fragment_button, R.id.fragment_first_circle),
+                        null,
+                        ConstraintWidget.CHAIN_SPREAD)
 
                 animatedConstraints.applyTo(constraintLayout)
             }
