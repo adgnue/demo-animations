8229d4bbaa752cc5ef335d4427c111951ec1f16d
itsmortoncornelius <mortoncornelius@gmail.com>
2017-11-07 22:15:25 +0100

    Added all transitions

--- app/src/main/java/de/handler/mobile/constraintlayout/ui/SecondFragment.kt
+++ app/src/main/java/de/handler/mobile/constraintlayout/ui/SecondFragment.kt
@@ -32,7 +32,75 @@ class SecondFragment : BackAwareFragment() {
         val transitionSet = sharedElementEnterTransition as TransitionSet?
         transitionSet?.addListener(object : TransitionEndListener() {
             override fun onTransitionEnd(transition: Transition) {
-                // TODO Add your animations here
+                val animatedConstraints = ConstraintSet()
+                animatedConstraints.clone(constraintLayout)
+
+                TransitionManager.beginDelayedTransition(constraintLayout)
+                // augment right button margin animation
+                animatedConstraints.setMargin(R.id.fragment_second_switch_fragment_button, ConstraintSet.END, 128)
+
+                // center button vertically animation
+//                animatedConstraints.centerVertically(R.id.fragment_second_switch_fragment_button, R.id.fragment_second_circle)
+
+                // grow width & height animation
+//                fragment_second_switch_fragment_button?.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+//                animatedConstraints.constrainWidth(R.id.fragment_second_switch_fragment_button, 1500)
+//                animatedConstraints.constrainWidth(R.id.fragment_second_circle, 2500)
+//                animatedConstraints.constrainHeight(R.id.fragment_second_circle, 2500)
+
+                // center in circle animation
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.START,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.START, 16)
+//                animatedConstraints.centerVertically(R.id.fragment_second_switch_fragment_button, R.id.fragment_second_circle)
+
+                // grow button to full screen size
+//                fragment_second_switch_fragment_button?.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+//                animatedConstraints.clear(R.id.fragment_second_switch_fragment_button)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.START,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.START, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.END,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.END, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.TOP,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.TOP, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.BOTTOM,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.BOTTOM, 0)
+
+                // create a chain of the two elements and align them next to each other
+                // reset constraints and set height / width to necessary values
+//                animatedConstraints.clear(R.id.fragment_second_switch_fragment_button)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.LEFT,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.LEFT, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.RIGHT,
+//                        R.id.fragment_second_circle, ConstraintSet.LEFT, 0)
+//                animatedConstraints.constrainWidth(R.id.fragment_second_switch_fragment_button, 0)
+//                animatedConstraints.constrainHeight(R.id.fragment_second_switch_fragment_button, ConstraintSet.WRAP_CONTENT)
+//
+//                animatedConstraints.clear(R.id.fragment_second_circle)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_circle, ConstraintSet.LEFT,
+//                        R.id.fragment_second_switch_fragment_button, ConstraintSet.RIGHT, 0)
+//                animatedConstraints.connect(
+//                        R.id.fragment_second_circle, ConstraintSet.RIGHT,
+//                        R.id.fragment_second_constraint_layout, ConstraintSet.RIGHT, 0)
+//                animatedConstraints.constrainWidth(R.id.fragment_second_circle, 0)
+//                animatedConstraints.constrainHeight(R.id.fragment_second_circle, ConstraintSet.WRAP_CONTENT)
+//
+//                // create chain
+//                animatedConstraints.createHorizontalChain(
+//                        ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
+//                        ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
+//                        intArrayOf(R.id.fragment_second_switch_fragment_button, R.id.fragment_first_circle),
+//                        null,
+//                        ConstraintWidget.CHAIN_SPREAD)
+
+                animatedConstraints.applyTo(constraintLayout)
             }
         })
     }
